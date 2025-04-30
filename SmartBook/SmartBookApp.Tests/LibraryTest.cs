04e0944e95249ef2f6c946f7cd7ed1bfc8bda587
OlenaKut <105447315+OlenaKut@users.noreply.github.com>
2025-04-30 19:06:44 +0200

    Added tests

--- SmartBook/SmartBookApp.Tests/LibraryTest.cs
+++ SmartBook/SmartBookApp.Tests/LibraryTest.cs
@@ -22,4 +22,64 @@ public class LibraryTest
         Assert.Contains(book, library.Books);
 
     }
+
+    [Fact]
+    public void RemoveBook_ShouldRemoveBookFromList()
+    {
+        // Arrange
+        var library = new Library();
+        var book = new Book("Test Title", "Test Author", "1234567890", "Fiction");
+        library.AddBook(book);
+
+        // Act
+        library.RemoveBook("1234567890");
+
+        // Assert
+        Assert.DoesNotContain(book, library.Books);
+
+    }
+
+    [Fact]
+    public void FindBook_ShouldFindBookInList()
+    {
+        // Arrange
+        var library = new Library();
+        var book = new Book("Test Title", "Test Author", "1234567890", "Fiction");
+        library.AddBook(book);
+
+        // Act
+        var result = library.FindBook("Olena");
+
+        // Assert
+        Assert.DoesNotContain(book, result);
+
+    }
+
+    [Fact]
+    public void LoanBook_ShouldMarkBookAsLoaned()
+    {
+        // Arrange
+        var library = new Library();
+        var book = new Book("Test Title", "Test Author", "1234567890", "Fiction");
+        library.AddBook(book);
+
+        // Act
+        library.LoanBook("1234567890");
+
+        // Assert
+        Assert.True(book.IsLoaned);
+    }
+
+    [Fact]
+    public void MarkAsAvailable_ShouldThrowIfNotLoaned()
+    {
+        // Arrange
+        var library = new Library();
+        var book = new Book("Test Title", "Test Author", "1234567890", "Fiction");
+        library.AddBook(book);
+
+
+        var ex = Assert.Throws<ArgumentException>(() => library.MarkAsAvailable("1234567890"));
+        Assert.Equal("Book is not currently loaned.", ex.Message);
+    }
 }
--- SmartBook/SmartBookApp.Tests/obj/Debug/net9.0/SmartBookApp.Tests.AssemblyInfo.cs
+++ SmartBook/SmartBookApp.Tests/obj/Debug/net9.0/SmartBookApp.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartBookApp.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67955fabacca5e6cdd6d4dbed9b1b2fcfb51d93f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a2bbf6f5e5357648a57f3026f376b2610f21580")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartBookApp.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartBookApp.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
