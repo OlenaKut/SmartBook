1a2bbf6f5e5357648a57f3026f376b2610f21580
OlenaKut <105447315+OlenaKut@users.noreply.github.com>
2025-04-30 17:54:00 +0200

    Added test AddBook_ShouldAddBookToList

---
+++ SmartBook/SmartBookApp.Tests/LibraryTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xunit;
+
+using SmartBookApp.Models;
+using SmartBookApp.Services;
+
+namespace SmartBookApp.Tests;
+
+public class LibraryTest
+{
+    [Fact]
+    public void AddBook_ShouldAddBookToList()
+    {
+        // Arrange
+        var library = new Library();
+        var book = new Book("Test Title", "Test Author", "1234567890", "Fiction");
+
+        // Act
+        library.AddBook(book);
+
+        // Assert
+        Assert.Contains(book, library.Books);
+
+    }
+}
